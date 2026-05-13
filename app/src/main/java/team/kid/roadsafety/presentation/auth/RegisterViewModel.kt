f53dd004233f4442f6868930020e9cd6929dfb9a
ilya034 <68101303+ilya034@users.noreply.github.com>
2026-05-13 14:50:11 +0500

    Rework project structure

--- app/src/main/java/team/kid/roadsafety/presentation/auth/RegisterViewModel.kt
+++ app/src/main/java/team/kid/roadsafety/presentation/auth/RegisterViewModel.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import team.kid.roadsafety.domain.enums.FamilyRole
+import team.kid.roadsafety.domain.aggregates.family.FamilyRole
 
 data class RegisterState(
     val login: String = "",
